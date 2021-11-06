dee4868ce2f1f1caa3dc823f00475fe9be45f4ed
Dawid Bulinski (PGS Software) <dbulinski@pgs-soft.com>
2021-11-06 21:26:58 +0100

    Added initial code state

---
+++ app/routes/admin/posts.$id.tsx
@@ -0,0 +1,5 @@
+import { PostForm } from "../../components/PostForm";
+
+export function PostsUpdater() {
+  return <PostForm disabled={false} buttonText="Update" />;
+}
---
+++ app/routes/admin/posts.add.tsx
@@ -0,0 +1,5 @@
+import { PostForm } from "../../components/PostForm";
+
+export function PostsAdder() {
+  return <PostForm disabled={false} buttonText="Add" />;
+}
