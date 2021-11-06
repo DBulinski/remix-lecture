7c2a56b6bba4f19ad1c3ea27539bb5f7bef6c78a
Dawid Bulinski (PGS Software) <dbulinski@pgs-soft.com>
2021-11-06 22:39:46 +0100

    Added loader

--- app/routes/admin/posts.$id.tsx
+++ app/routes/admin/posts.$id.tsx
@@ -1,7 +1,16 @@
-import { LinksFunction, MetaFunction } from "remix";
+import {
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+  useLoaderData,
+} from "remix";
 import { PostForm } from "../../components/PostForm";
+import { Post, postsService } from "../../services/postsService";
 
 import postFormCss from "../../styles/admin/postForm.css";
+
+export const loader: LoaderFunction = ({ params }) =>
+  postsService.getOne(Number(params.id));
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: postFormCss },
@@ -12,5 +21,7 @@ export const meta: MetaFunction = () => ({
 });
 
 export default function PostsUpdater() {
-  return <PostForm disabled={false} buttonText="Update" />;
+  const post = useLoaderData<Post>();
+
+  return <PostForm initialValues={post} disabled={false} buttonText="Update" />;
 }
