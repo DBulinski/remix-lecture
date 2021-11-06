42fc855cdf8c1697917e62c870ac4f8deab13e9f
Dawid Bulinski (PGS Software) <dbulinski@pgs-soft.com>
2021-11-06 22:37:38 +0100

    Prepared initial state for step/3

--- app/routes/admin/posts.$id.tsx
+++ app/routes/admin/posts.$id.tsx
@@ -1,4 +1,15 @@
+import { LinksFunction, MetaFunction } from "remix";
 import { PostForm } from "../../components/PostForm";
+
+import postFormCss from "../../styles/admin/postForm.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: postFormCss },
+];
+
+export const meta: MetaFunction = () => ({
+  title: "Update post",
+});
 
 export default function PostsUpdater() {
   return <PostForm disabled={false} buttonText="Update" />;
--- app/routes/admin/posts.add.tsx
+++ app/routes/admin/posts.add.tsx
@@ -1,4 +1,15 @@
+import { LinksFunction, MetaFunction } from "remix";
 import { PostForm } from "../../components/PostForm";
+
+import postFormCss from "../../styles/admin/postForm.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: postFormCss },
+];
+
+export const meta: MetaFunction = () => ({
+  title: "Add new post",
+});
 
 export default function PostsAdder() {
   return <PostForm disabled={false} buttonText="Add" />;
